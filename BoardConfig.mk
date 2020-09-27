e877a70de0948ae94044a88429754a225465701a
somadsul <somnathadsul@gmail.com>
2020-09-27 16:10:16 +0530

    jackpot2lte: Bluetooth: device name

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -10,6 +10,9 @@ TARGET_OTA_ASSERT_DEVICE := jackpot2ltexx,jackpot2lte
 # Kernel
 TARGET_KERNEL_CONFIG :=exynos7885-jackpot2lte_defconfig
 
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth/include
+
 # Display
 TARGET_SCREEN_DENSITY := 420
 
